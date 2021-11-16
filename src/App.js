bc93a0fdef599b1d2487b9326817cbcdc6dc942d
Test <test@flex.server>
2021-11-16 14:58:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState, useRef } from "react";
+import useRandomJoke from "./useRandomJoke";
 
 function App() {
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+
+  const [firstName, setFirstName] = useState("James");
+  const [lastName, setlastName] = useState("Taylor");
+  const [generate, setGenerate] = useState(false);
+
+  const joke = useRandomJoke(firstName, lastName, generate);
+
+  const generateJoke = (e) => {
+    e.preventDefault();
+    setFirstName(firstNameRef.current.value);
+    setlastName(lastNameRef.current.value);
+    setGenerate(!generate);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The API Joke Generator</h1>
+      <strong>{joke !== null ? joke : ""}</strong>
+      <br />
+      <form>
+        <input type="text" placeholder="first name" ref={firstNameRef} />
+        <input type="text" placeholder="last name" ref={lastNameRef} />
+      </form>
+      <button onClick={generateJoke}>Generate Joke</button>
     </div>
   );
 }
